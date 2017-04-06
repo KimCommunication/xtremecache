8dfde866cbf04f69f2521c72a541242974887c99
pdurko <pdurko@users.noreply.github.com>
2017-04-06 18:38:52 +0200

    Modification to allow external application delete & regenerate specific product cache separately

--- xtremecache.php
+++ xtremecache.php
@@ -8,41 +8,14 @@
  */
 
 require __DIR__.DS.'vendor'.DS.'phpfastcache.php';
-
+require __DIR__.DS.'config.php';
 
 class XtremeCache extends Module {
-    /**
-     * Cache Time-To-Live in seconds
-     * Since cache gets cleaned quite often, use a very high value (!removed admin performance override)
-     */
-    const CACHE_TTL = 172800;
-    
-    /**
-     * Cache driver
-     */
-    const DRIVER = 'files';
-	
-	/**
-     * Cache mobile and desktop versions separatelly?
-     */
-    const SEPARATE_MOBILE_AND_DESKTOP = false;
-    
-	/**
-     * True add one more DB query to each reqest.
-	 * Clear cache after changing this value, because
-	 * "maintenance" cached pages can still be served if this value
-	 * is currently false and shop has been prieviously in maintenance mode,
-	 * or opposite.
-	 * If maintenance mode is On and this value true, cache will be off.
-     */
-    const CHECK_FOR_MAINTENANCE = false;
-	
     /**
      * Cache engine
      * @var BasePhpFastCache
      */
     private $fast_cache;
-    
     
     public function __construct() {
         $this->name = 'xtremecache';
@@ -111,10 +84,10 @@ class XtremeCache extends Module {
         //if not in the checkout process
         if ($params['controller_class'] !== 'OrderController' && 
             $params['controller_class'] !== 'OrderOpcController')
-		{
+        {
             $cached = $this->fast_cache->get($this->getCacheKey());
             
-            if (NULL !== $cached) {
+            if (null !== $cached) {
                //empty output buffer
                ob_get_clean();
                die($cached);
@@ -129,73 +102,73 @@ class XtremeCache extends Module {
     public function hookActionRequestComplete($params) {
         if (!$this->isActive())
             return;
-		
+        
         $controller = $params['controller'];
         
         if (is_subclass_of($controller, 'FrontController') &&
             !is_subclass_of($controller, 'OrderController') &&
             !is_subclass_of($controller, 'OrderOpcController')) {
-			
+            
             $key = $this->getCacheKey();
             //mark page as cached
             $debugInfo = sprintf(
-            	"<!-- %s from %s on %s] -->
+                "<!-- %s from %s on %s] -->
 ",
-            	$key,
-            	static::DRIVER,
-            	date('Y-m-d H:i:s'));
-			
+                $key,
+                DRIVER,
+                date('Y-m-d H:i:s'));
+            
             $output = $debugInfo . $params['output'];
-            $this->fast_cache->set($key, $output, static::CACHE_TTL);		
+            $this->fast_cache->set($key, $output, CACHE_TTL);        
         }
     }
-	
-	public function hookActionEmptySmartyCache($params) {
-		$this->fast_cache->clean();
-	}
+    
+    public function hookActionEmptySmartyCache($params)
+    {
+        $this->fast_cache->clean();
+    }
 
     /**
      * Check if should use cache
-	 * checks for: dev mode, profilling, front controller, maintenance mode?, customer, shopping cart, AJAX and POST requests
+     * checks for: dev mode, profilling, front controller, maintenance mode?, customer, shopping cart, AJAX and POST requests
      * @return boolean
      */
     private function isActive()
-	{
+    {
         //turn off on debug mode
         if (_PS_MODE_DEV_ || _PS_DEBUG_PROFILING_)
             return false;
         
-		//turn off if it is not front office
-		if(!("front" === $this->context->controller->controller_type))
-			return false;
-		
-		//disable on ajax and non-GET requests
+        //turn off if it is not front office
+        if(!("front" === $this->context->controller->controller_type))
+            return false;
+        
+        //disable on ajax and non-GET requests
         //$active = !Tools::getValue('ajax', false);
         $active = !(isset($this->ajax) ? $this->ajax : false);
         $active = $active && $_SERVER['REQUEST_METHOD'] === 'GET';
-		if (!$active)
-			return false;
-		
-		// are we going to check if maintenance mode is on?
-		// if enabled, during maintenance mode there will be no cache
-		if (static::CHECK_FOR_MAINTENANCE && !((bool)Configuration::get('PS_SHOP_ENABLE', true)))
-			return false;
-		
+        if (!$active)
+            return false;
+        
+        // are we going to check if maintenance mode is on?
+        // if enabled, during maintenance mode there will be no cache
+        if (CHECK_FOR_MAINTENANCE && !((bool)Configuration::get('PS_SHOP_ENABLE', true)))
+            return false;
+        
         //check that customer is not logged in
         $customer = $this->context->customer;
         if ($customer && $customer instanceof Customer && $customer->id > 0)
             return false;
         
         //for guest checkout, check that cart is empty
-		//global $cookie;
-		//$cart = new Cart($cookie->id_cart);
-		if (isset($this->context->cart))
-		{
-			$cart = $this->context->cart;
-			if ($cart && $cart instanceof Cart && $cart->nbProducts() > 0)
-				return false;
+        //global $cookie;
+        //$cart = new Cart($cookie->id_cart);
+        if (isset($this->context->cart))
+        {
+            $cart = $this->context->cart;
+            if ($cart && $cart instanceof Cart && $cart->nbProducts() > 0)
+                return false;
         }
-		
         return true;
     }
     
@@ -204,28 +177,28 @@ class XtremeCache extends Module {
      * @return BasePhpFastCache 
      */
     private function getFastCache() {
-        phpFastCache::setup('path', __DIR__.DS.'xcache');
-        return phpFastCache(static::DRIVER);
+        phpFastCache::setup('path', __DIR__ . DS . CACHE_DIR);
+        return phpFastCache(DRIVER);
     }
     
     /**
      * Map url to cache key
      * @return string 
      */
-    private function getCacheKey($url=NULL)
-	{
-        if ($url === NULL)
+    private function getCacheKey($url = null)
+    {
+        if ($url === null)
             $url = $_SERVER['REQUEST_URI'];
         
-		if (static::SEPARATE_MOBILE_AND_DESKTOP)
-			$url = 'device-'.$this->context->getDevice().
-					'-lang-'.$this->context->language->id.
-					'-shop-'.$this->context->shop->id.'-'.
-					$url;
-		else
-			$url = 'lang-'.$this->context->language->id.
-					'-shop-'.$this->context->shop->id.'-'.
-					$url;
+        if (SEPARATE_MOBILE_AND_DESKTOP)
+            $url = 'device-'.$this->context->getDevice().
+                    '-lang-'.$this->context->language->id.
+                    '-shop-'.$this->context->shop->id.'-'.
+                    $url;
+        else
+            $url = 'lang-'.$this->context->language->id.
+                    '-shop-'.$this->context->shop->id.'-'.
+                    $url;
         
         $url = md5($url);
         return $url;
